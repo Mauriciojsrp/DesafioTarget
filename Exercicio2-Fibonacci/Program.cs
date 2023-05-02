8cc31276bd38f05bb6ce8979d38e89f58bde9659
mauriciojsrp <mauriciojsrp@gmail.com>
2023-05-02 14:37:27 -0300

    Enviando projeto para o repositório

---
+++ Exercicio2-Fibonacci/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Digite um número: ");
+int valorEntrada = int.Parse(Console.ReadLine());
+
+int n1 = 0, n2 = 1;
+int SOMA = 0;
+bool ehFibonacci = false;
+
+while (SOMA < valorEntrada)
+{
+    SOMA = n1 + n2;
+    n1 = n2;
+    n2 = SOMA;
+
+    if (SOMA == valorEntrada)
+        ehFibonacci = true;
+}
+
+if (ehFibonacci)
+    Console.WriteLine("O número informado pertence a sequencia de Fibonacci");
+else
+    Console.WriteLine("O número informado NÃO pertence a sequencia de Fibonacci");
+
---
+++ Exercicio5-InverteString/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Digite a String: ");
+char[] texto = Console.ReadLine().ToCharArray();
+
+for (int i = 0; i < texto.Length / 2; i++)
+{
+    var caracter = texto[i];
+    texto[i] = texto[texto.Length - i - 1];
+    texto[texto.Length - i - 1] = caracter;
+}
+
+Console.WriteLine($"A string invertida é: {new string(texto)}");
